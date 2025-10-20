478a56e3e202d661c52c7d4f3f0b6093ce650442
P09XD371 <rustrustleva@gmail.com>
2025-10-20 19:46:19 +0500

    СОЗДАЛ ЦИКЛ

--- script.py
+++ script.py
@@ -7,12 +7,14 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    for _ in range(5):
+        print("ALINA")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("Hello world")
     print_hi('PyCharm')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
